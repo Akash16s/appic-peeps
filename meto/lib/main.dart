ec81bd55e3efca140c34f3e27ff6e6aa2f214e0c
chetan <chetanverma99.cv@gmail.com>
2019-08-23 20:16:22 +0530

    ref

---
+++ meto/lib/main.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'form1.dart';
+
+void main(){
+   SystemChrome.setPreferredOrientations(
+      [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
+  SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+    systemNavigationBarColor: Colors.black,
+    statusBarColor: Color(0xFFFA9F56),
+  ));
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WelcomeScreen(),
+    );
+  }
+}
+
+class WelcomeScreen extends StatefulWidget{
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return WelcomeScreenState();
+  }
+}
+class WelcomeScreenState extends State<WelcomeScreen>{
+  Widget build(BuildContext context){
+    return Scaffold(
+      body: Container(
+        color: Color(0xFFF8913A),
+        // color: Color(0xFFF77B00),
+        child: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Text('Follow the path to agree our',style: TextStyle(color: Colors.white.withOpacity(1),fontWeight: FontWeight.bold,fontSize: 20),),
+              Padding(padding: EdgeInsets.only(top: 5),),
+              Text('Terms and Privacy Policy',style: TextStyle(color: Colors.white.withOpacity(1),fontWeight: FontWeight.bold,fontSize: 20),),
+              Padding(padding: EdgeInsets.only(top: 105)),
+              SizedBox(
+                width: 300,
+                height: 50,
+                 child: RaisedButton(
+                   color: Colors.white,
+                  child: Text('Get Started',style: TextStyle(color: Color(0xFFF8913A)),),
+                  onPressed: (){
+                    Navigator.push(context, MaterialPageRoute(builder: (context)=>Form1()));
+                  },
+                  shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)),
+                ),
+              ),
+
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
